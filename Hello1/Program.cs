13d7f13c7fb9679a13dc52aef635b38618ebb22e
Zenab WaglaWala <Zenab.Waglawala@Advaiya.com>
2021-09-06 11:54:29 +0530

    Add project files.

---
+++ Hello1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
